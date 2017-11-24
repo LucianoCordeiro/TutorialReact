809751faaf9d3fca8cd6c51ac520f1e5b70a0485
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2017-11-24 00:42:18 -0200

    Learning dynamic props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+ class CommentBox extends React.Component {
+   render() {
+     const comments = this._getComments();
+     const numberofcomments = this._getCommentsCount(comments.length);
+     return( <div className="comment-box">
+              <h3>Comments</h3>
+              <h4 className="comment-count">{numberofcomments}</h4>
+              <div className="comment-list">
+              {comments}
+                </div>
+           </div>
+         );
+   }
+
+   _getComments() {
+     const commentList = [
+       {id: 1, author: "Luciano Cesar", body:"We do not have another way to live, just that"},
+       {id: 2, author: "Dodo Cesar", body:"Guess what I have said to my black cat"}
+
+     ];
+
+     return commentList.map((comment) => {
+       return (<Comment
+               author={comment.author}
+               body={comment.body}
+               key={comment.id} />);
+       });
+       }
+
+    _getCommentsCount(commentCount) {
+      if (commentCount === 0) {
+        return "No comments yet";
+      }
+      else if (commentCount === 1) {
+        return "1 comment";
+      }
+      else {
+        return commentCount + " comments";
+      }
+
+   }
+ }
+
+
+
+ class Comment extends React.Component {
+   render() {
+     return(
+       <div className="comment">
+       <p className="comment-header">{this.props.author}</p>
+       <p className="comment-body">{this.props.body}</p>
+       <div className="footer">
+           <a href="#" className="comment-footer-delete">Delete comment</a>
+
+       </div>
+     </div>);
+   }
+ }
+
+ ReactDOM.render(
+  <CommentBox />, document.getElementById('commentbox')
+ );
+
+
+
+
 class UserData extends React.Component {
   render() {
     return <div>
@@ -22,8 +89,8 @@ ReactDOM.render(
 
 class Calculation extends React.Component {
   render() {
-    let pi = Math.PI
-    let exponentiation = 3**5
+    let pi = Math.PI;
+    let exponentiation = 3**5;
     return <div>The value of pi is approximately {pi}
            <h3>{exponentiation}</h3></div>
   }
@@ -31,4 +98,18 @@ class Calculation extends React.Component {
 
 ReactDOM.render(
   <Calculation />, document.getElementById('calculation')
+);
+
+class Learnedlanguages extends React.Component {
+  render() {
+    const programminglanguages = ["Ruby", "JavaScript", "PHP", "Python", "TypeScript", "Kotlin"];
+    return(
+      <ul>{programminglanguages.map( (language) => <li>{language}</li>)}
+      </ul>
+  );
+  }
+}
+
+ReactDOM.render(
+  <Learnedlanguages />, document.getElementById('learnedlanguages')
 );
